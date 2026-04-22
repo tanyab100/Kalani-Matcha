ff4968e8c203c39117845511145e0affe276b0f9
Tanyab100 <tsbakshi@calpoly.edu>
2026-04-22 15:31:55 -0700

    remove Nami branding

--- client/src/config/store.ts
+++ client/src/config/store.ts
@@ -1,7 +1,7 @@
 const address = "2836 El Capitan Drive, Pleasanton, CA 94566";
 
 export const STORE = {
-  name: "Nami Matcha",
+  name: "Ceremonial Grade Matcha",
   address,
   mapsUrl: `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(address)}`,
 };
